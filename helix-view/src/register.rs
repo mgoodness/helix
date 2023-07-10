fc82ba590ec6fc6099100d71a61835b079c610a1
Michael Davis <mcarsondavis@gmail.com>
2023-07-10 17:17:04 -0500

    Add system & primary clipboards as special registers
    These special registers join and copy the values to the clipboards with
    '*' corresponding to the system clipboard and '+' to the primary as
    they are in Vim. This also uses the trick from PR6889 to save the values
    in the register and re-use them without joining into one value when
    pasting a value which was yanked and not changed.
    
    These registers are not implemented in Kakoune but Kakoune also does
    not have a built-in clipboard integration.
    
    Co-authored-by: CcydtN <51289140+CcydtN@users.noreply.github.com>
    Co-authored-by: Pascal Kuthe <pascal.kuthe@semimod.de>

--- helix-view/src/register.rs
+++ helix-view/src/register.rs
@@ -1,8 +1,13 @@
 use std::{borrow::Cow, collections::HashMap, iter};
 
 use anyhow::Result;
+use helix_core::NATIVE_LINE_ENDING;
 
-use crate::{document::SCRATCH_BUFFER_NAME, Editor};
+use crate::{
+    clipboard::{get_clipboard_provider, ClipboardProvider, ClipboardType},
+    document::SCRATCH_BUFFER_NAME,
+    Editor,
+};
 
 /// A key-value store for saving sets of values.
 ///
@@ -14,9 +19,21 @@ use crate::{document::SCRATCH_BUFFER_NAME, Editor};
 /// * Selection indices (`#`): index number of each selection starting at 1
 /// * Selection contents (`.`)
 /// * Document path (`%`): filename of the current buffer
-#[derive(Debug, Default)]
+/// * System clipboard (`*`)
+/// * Primary clipboard (`+`)
+#[derive(Debug)]
 pub struct Registers {
     inner: HashMap<char, Vec<String>>,
+    clipboard_provider: Box<dyn ClipboardProvider>,
+}
+
+impl Default for Registers {
+    fn default() -> Self {
+        Self {
+            inner: Default::default(),
+            clipboard_provider: get_clipboard_provider(),
+        }
+    }
 }
 
 // Some special registers must allocate their values while others and regular
@@ -52,6 +69,15 @@ impl Registers {
 
                 Some(Box::new(iter::once(path)))
             }
+            '*' | '+' => Some(read_from_clipboard(
+                self.clipboard_provider.as_ref(),
+                self.inner.get(&name),
+                match name {
+                    '*' => ClipboardType::Clipboard,
+                    '+' => ClipboardType::Selection,
+                    _ => unreachable!(),
+                },
+            )),
             _ => self
                 .inner
                 .get(&name)
@@ -63,6 +89,18 @@ impl Registers {
         match name {
             '_' => Ok(()),
             '#' | '.' | '%' => Err(anyhow::anyhow!("Register {name} does not support writing")),
+            '*' | '+' => {
+                self.clipboard_provider.set_contents(
+                    values.join(NATIVE_LINE_ENDING.as_str()),
+                    match name {
+                        '*' => ClipboardType::Clipboard,
+                        '+' => ClipboardType::Selection,
+                        _ => unreachable!(),
+                    },
+                )?;
+                self.inner.insert(name, values);
+                Ok(())
+            }
             _ => {
                 self.inner.insert(name, values);
                 Ok(())
@@ -74,6 +112,28 @@ impl Registers {
         match name {
             '_' => Ok(()),
             '#' | '.' | '%' => Err(anyhow::anyhow!("Register {name} does not support pushing")),
+            '*' | '+' => {
+                let clipboard_type = match name {
+                    '*' => ClipboardType::Clipboard,
+                    '+' => ClipboardType::Selection,
+                    _ => unreachable!(),
+                };
+
+                let mut values: Vec<_> = read_from_clipboard(
+                    self.clipboard_provider.as_ref(),
+                    self.inner.get(&name),
+                    clipboard_type,
+                )
+                .map(|value| value.to_string())
+                .collect();
+                values.push(value);
+
+                self.clipboard_provider
+                    .set_contents(values.join(NATIVE_LINE_ENDING.as_str()), clipboard_type)?;
+                self.inner.insert(name, values);
+
+                Ok(())
+            }
             _ => {
                 self.inner.entry(name).or_insert_with(Vec::new).push(value);
                 Ok(())
@@ -92,6 +152,7 @@ impl Registers {
     pub fn iter_preview(&self) -> impl Iterator<Item = (char, &str)> {
         self.inner
             .iter()
+            .filter(|(name, _)| !matches!(name, '*' | '+'))
             .map(|(name, values)| {
                 let preview = values
                     .first()
@@ -106,6 +167,8 @@ impl Registers {
                     ('#', "<selection indices>"),
                     ('.', "<selection contents>"),
                     ('%', "<document path>"),
+                    ('*', "<system clipboard>"),
+                    ('+', "<primary clipboard>"),
                 ]
                 .iter()
                 .copied(),
@@ -118,8 +181,62 @@ impl Registers {
 
     pub fn remove(&mut self, name: char) -> bool {
         match name {
-            '_' | '#' | '.' | '%' => false,
+            '_' | '#' | '.' | '%' | '*' | '+' => false,
             _ => self.inner.remove(&name).is_some(),
         }
     }
+}
+
+fn read_from_clipboard<'a>(
+    provider: &dyn ClipboardProvider,
+    saved_values: Option<&'a Vec<String>>,
+    clipboard_type: ClipboardType,
+) -> RegisterValues<'a> {
+    match provider.get_contents(clipboard_type) {
+        Ok(contents) => {
+            // If we're pasting the same values that we just yanked, re-use
+            // the saved values. This allows pasting multiple selections
+            // even when yanked to a clipboard.
+            let Some(values) = saved_values else { return Box::new(iter::once(contents.into())) };
+
+            if contents_are_saved(values, &contents) {
+                Box::new(values.iter().map(Cow::from))
+            } else {
+                Box::new(iter::once(contents.into()))
+            }
+        }
+        Err(err) => {
+            log::error!(
+                "Failed to read {} clipboard: {err}",
+                match clipboard_type {
+                    ClipboardType::Clipboard => "system",
+                    ClipboardType::Selection => "primary",
+                }
+            );
+
+            Box::new(iter::empty())
+        }
+    }
+}
+
+fn contents_are_saved(saved_values: &[String], mut contents: &str) -> bool {
+    let line_ending = NATIVE_LINE_ENDING.as_str();
+    let mut values = saved_values.iter();
+
+    match values.next() {
+        Some(first) if contents.starts_with(first) => {
+            contents = &contents[first.len()..];
+        }
+        _ => return false,
+    }
+
+    for value in values {
+        if contents.starts_with(line_ending) && contents[line_ending.len()..].starts_with(value) {
+            contents = &contents[line_ending.len() + value.len()..];
+        } else {
+            return false;
+        }
+    }
+
+    true
 }
